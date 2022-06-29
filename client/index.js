e8ff55a89271aa8e61ae4d8b7accbf8095b06d14
Maxwell Shick <maxwellshick@gmail.com>
2022-06-28 22:16:15 -0400

    Finished the boilerplate

---
+++ client/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+// import styles from './styles/styles.scss'; 
+
+render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root')
+);
