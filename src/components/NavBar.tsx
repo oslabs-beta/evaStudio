e74eca1697fa8769956c6cc3ae438d881c47621f
Sattwik <hellosattwik@gmail.com>
2022-07-06 01:24:22 +0530

    added icon

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import LogoIcon from '../assets/logo.svg';
 
 const NavBar = () => {
   return (
     <div id='navBar'>
-      <h1>EvaStudio</h1>
+      <img src={LogoIcon} alt="" />
     </div>
   )
 }
