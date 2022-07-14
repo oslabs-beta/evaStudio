f0267facf98cf496159f43f309f0055b91cb5422
Kai Zhang <kaizen.agility@gmail.com>
2022-07-14 15:47:22 -0400

    added kafka clusters streaming from Wikimedia

---
+++ wikimedia_stream/src/main/java/evaStudio/wikimedia/WikimediaKafkaApplication.java
@@ -0,0 +1,13 @@
+package evaStudio.wikimedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikimediaKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikimediaKafkaApplication.class, args);
+	}
+
+}
