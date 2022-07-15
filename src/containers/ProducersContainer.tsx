6e301a19cbbb0f3a0b56e6870f68268f08fd1bd5
Sattwik <79150484+sattwyk@users.noreply.github.com>
2022-07-15 21:37:54 +0530

    Revert "Perla"

---
+++ src/containers/ProducersContainer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProducersContainer = () => {
+  return (
+    <div id='producersContainer'>
+      <h1>Producers Container</h1>
+    </div>
+  )
+}
+
+export default ProducersContainer;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store.ts';
+// import styles from './styles/styles.scss';
+import globals from './styles/globals.css';
+
+render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
