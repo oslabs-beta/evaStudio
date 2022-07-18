7e89a6f11f1a1a4fdda91b7759eef1851950e2ef
Andres Felipe Chaparro Carvajal <andreschc9@gmail.com>
2022-07-18 11:00:29 -0500

    Changed the directory for creating the docker compose yml file

--- server/routes/createClustersRoute.js
+++ server/routes/createClustersRoute.js
@@ -4,7 +4,7 @@ const path = require('path');
 const Router = express.Router();
 
 Router.post('/', createClustersController.launchContainers, (req, res) => {
-  return res.status(200).download(path.resolve(__dirname,'../../config/docker/docker-compose.yml'), 'docker-compose.yml');
+  return res.status(200).download(path.resolve(__dirname,'../../config/docker-compose.yml'), 'docker-compose.yml');
 });
 
 module.exports = Router;
