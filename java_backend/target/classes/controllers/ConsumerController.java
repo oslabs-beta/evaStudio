38ee485d44508c8d47ef38d035ebb0cb6dc4c108
Kai Zhang <kaizen.agility@gmail.com>
2022-07-20 02:31:01 -0400

    stashing changes

--- java_backend/target/classes/controllers/ConsumerController.java
+++ java_backend/target/classes/controllers/ConsumerController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Collections;
 
 
 @RestController
@@ -17,22 +18,20 @@ public class ConsumerController {
 
     public ConsumerController(KafkaConsumer kafkaConsumer){this.kafkaConsumer = kafkaConsumer;}
 
-//     @GetMapping("/consume")
-//    @KafkaListener(topics = "httpTopic", groupId = "http")
 //     public ResponseEntity<String> data(@RequestParam("topic") String topic) {
 //         return ResponseEntity.ok("Data sent to server");
 //     }
+       @GetMapping("/consume")
+    public ResponseEntity<String> data() {
+        final KafkaConsumer<String, String> consumer = new kafkaConsumer<>();
 
-//     public ResponseEntity<String> data() {
-//         final KafkaConsumer<String, String> consumer = new KafkaConsumer<>();
-//
-//        consumer.subscribe(Collections.singletonList(javaguides));
-//        return ResponseEntity.ok("Data sent to server");
-//
-//        final ConsumerRecords<Long, String> consumerRecords = consumer.poll(numMessages);
-//
-//        final ConsumerRecords<String, String>
-//     }
+       consumer.subscribe(Collections.singletonList(TOPIC));
+       return ResponseEntity.ok("Data sent to server");
+
+       final ConsumerRecords<Long, String> consumerRecords = consumer.poll(numMessages);
+
+       final ConsumerRecords<String, String>
+    }
 
 }
 
