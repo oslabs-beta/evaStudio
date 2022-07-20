71c21885223c0e4d881651e59b8204a497697ead
Kai Zhang <kaizen.agility@gmail.com>
2022-07-20 03:04:17 -0400

    separate dev/prod/local env

--- java_backend/src/main/java/evastudio/config/KafkaTopicConfig.java
+++ java_backend/src/main/java/evastudio/config/KafkaTopicConfig.java
@@ -1,21 +1,27 @@
-package evastudio;
+package evastudio.config;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
+@Profile("local")
 public class KafkaTopicConfig {
 
     @Bean
     public NewTopic httpTopic() {
         return TopicBuilder.name("httpTopic")
+                .partitions(3)
+                .replicas(3)
                 .build();
     }
 
     @Bean
       public NewTopic jsonTopic() {
             return TopicBuilder.name("jsonTopic")
+                    .partitions(3)
+                    .replicas(3)
                     .build();
         }
 
