5e442e810ed95ba2043a1e9c813670f14cb6a84b
Kai Zhang <kaizen.agility@gmail.com>
2022-07-20 09:46:40 -0400

    bork, commented out

--- java_backend/src/test/java/evastudio/integration/producer/IntegrationTest.java
+++ java_backend/src/test/java/evastudio/integration/producer/IntegrationTest.java
@@ -2,17 +2,23 @@ package evastudio.integration.controller;
 
 import evastudio.kafka.KafkaConsumer;
 import evastudio.kafka.KafkaProducer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.annotation.DirtiesContext;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+
+// Tells Spring that the application context created as part of this test is dirty and should be cleared after the test is finished
 @DirtiesContext
+
+// Provides an in-memory Kafka instance in Spring-based tests, and creates an instance of EmbeddedKafkaBroker bean that can be injected and used in tests
 @EmbeddedKafka(partitions = 2, brokerProperties = {"listeners=PLAINTEXT:localhost:9092", "port=9092"})
 
 public class IntegrationTest {
@@ -34,5 +40,4 @@ public class IntegrationTest {
         boolean messageConsumed = consumer.consume();
         assertTrue(messageConsumed);
     }
-
 }
