e0a760d112815e9f5ccb33b952cc2f851cb79f47
Kai Zhang <kaizen.agility@gmail.com>
2022-07-20 03:38:53 -0400

    error handling methods

--- java_backend/src/main/java/evastudio/kafka/KafkaProducer.java
+++ java_backend/src/main/java/evastudio/kafka/KafkaProducer.java
@@ -2,8 +2,9 @@ package evastudio.kafka;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 @Service
 public class KafkaProducer {
 
@@ -16,9 +17,42 @@ public class KafkaProducer {
         this.kafkaTemplate = kafkaTemplate;
     }
 
-    public void sendMessage(String message){
+    public <onFailure> void sendMessage(String message){
+        ObjectMapper objectMapper;
+//        String value = objectMapper.writeValueAsString(Message);
 
         LOGGER.info(String.format("Message sent %s", message));
         kafkaTemplate.send("httpTopic", message);
+
+//        private onFailure Object Object Integer;
+//        java.lang.Object integer = Integer;
+//        Integer Integer key;
+//        Object String;
+//        key, String Object Throwable;
+//        value, Throwable Object ex;
+//        Object ex1 = ex;) {
+//            handleFailure(key, value);
+//        }
+//
+//        private onSuccess Object SendResult;
+//        Object result = null;
+//        (SendResult Integer, String result) {
+//            handleSuccess(key, value, result);
+//        }
     }
+
+//    private void handleFailure(Integer key, String value) {
+//        ch.qos.logback.classic.Logger log;
+//        log.error("Error Sending the Message. This is the exception: {}", ex.getMessage());
+//        try {
+//            throw ex;
+//        } catch (Throwable throwable) {
+//            log.error("Error is OnFailure: {}", throwable.getMessage());
+//        }
+//    }
+//
+//    private void handleSuccess(Integer key, String value, Throwable ex) {
+//        SendResult<Object, Object> result;
+//        log.info("Message Sent SuccessFully for the key : {} and the value is {} , partition is {}", key, value, result.getRecordMetadata().partition());
+//    }
 }
