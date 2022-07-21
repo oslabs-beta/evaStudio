3728bb85992f4563d7b2a67932dcdf8f313f1dd5
Sattwik <hellosattwik@gmail.com>
2022-07-21 12:42:53 +0530

    - added images to interactive nodes - implemented throttle function on createCluster
    Co-authored-by: Andres Felipe Chaparro <anch09@users.noreply.github.com>
    Co-authored-by: kaitlin 改善 <kaitlin.zhang@owasp.org>

--- src/components/InteractiveNode.tsx
+++ src/components/InteractiveNode.tsx
@@ -1,22 +1,40 @@
 import React from 'react'
+import jupyterLogo from '../assets/jupyter.svg';
+import kafkaLogo from '../assets/kafka.svg';
+import sparkLogo from '../assets/spark.svg';
+import postgresLogo from '../assets/postgres.svg';
+
 
 const flowCluster = (props: { type: string, kafkaCluster?: number }) => {
   const { type, kafkaCluster } = props;
-  let nodeLabel = '';
+  let nodeLabel;
+  let nodeImg;
 
-  if (type === 'postgresql') {
-    nodeLabel = 'PostgreSQL';
-  } else if (type === 'jupyter') {
-    nodeLabel = 'Jupyter Notebook';
-  } else if (type === 'spark') {
-    nodeLabel = 'Apache Spark';
-  } else if (type === 'Kafka') {
-    nodeLabel = `Kafka ${kafkaCluster}`;
-  } else nodeLabel = type;
+  switch(type) {
+  case 'postgresql':
+      nodeLabel = 'PostgreSQL';
+      nodeImg = postgresLogo;
+    break;
+  case 'jupyter':
+    // code block
+      nodeLabel = 'Jupyter Notebook'
+      nodeImg = jupyterLogo;
+      break;
+    case 'spark':
+      nodeLabel = 'Apache Spark'
+      nodeImg = sparkLogo;
+      break;
+  case 'Kafka':
+      nodeLabel = `Kafka ${kafkaCluster}`
+      nodeImg = kafkaLogo;
+    break;
+  default:
+    nodeLabel = type
+}
 
   return (
-    <div className='flowCluster bg-slate-500'>
-
+    <div className='flowCluster flex items-center justify-center gap-1'>
+      <img className='w-5 h-5' src={nodeImg} alt={nodeLabel} />
       <h1>{nodeLabel}</h1>
     </div>
   )
