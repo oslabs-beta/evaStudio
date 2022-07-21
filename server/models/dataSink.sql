0ae4f9a8620cb47aed13d50d7f463f32425319b6
Perla Royer <perlaroyerc@gmail.com>
2022-07-20 23:05:14 -0400

    Configured databse URI

--- server/models/dataSink.sql
+++ server/models/dataSink.sql
@@ -1,10 +1,21 @@
-CREATE TABLE data_sink.events (
+CREATE TABLE datasink.events (
   "_id" serial NOT NULL,
   "timestamp" varchar,
-  "message" varchar
-)
-
-CREATE TABLE data_sink.topics (
-  "_id" serial NOT NULL,
+  "message" varchar,
   "topic" varchar
-)
+);
+
+
+INSERT INTO datasink.events (timestamp, message)
+SELECT timestamp, value
+FROM messages;
+
+UPDATE datasink.events SET topic = 'jsonTopic' WHERE _id > 0;
+
+
+CREATE TABLE datasink.topics (
+  "_id" SERIAL PRIMARY KEY NOT NULL,
+  "topicname" VARCHAR NOT NULL,
+  "partitions" VARCHAR NOT NULL,
+  "replicas" VARCHAR NOT NULL
+);
